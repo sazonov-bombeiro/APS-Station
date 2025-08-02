1ef3b7d6705c94533d7f894a16e2c0097ba99d83
sazonov-bombeiro <geral.imobilizador@gmail.com>
2025-08-02 12:47:08 +0100

    Update APS_4M_TF.ino to work with ui_Screen1.c components
    - Updated updateModuleStyles function to properly map UI elements
    - Added switch statement to handle all 4 modules correctly
    - Mapped UI components: ui_Temp1-4, ui_Gray1-4, ui_Red1-4, etc.
    - Fixed opacity array handling for proper UI element control
    - Optimized code structure for better maintainability

--- APS_4M_TF.ino
+++ APS_4M_TF.ino
@@ -12,34 +12,25 @@ using namespace esp_panel::drivers;
 using namespace esp_panel::board;
 
 #include <Chrono.h>
-Chrono chrono1, chrono2, chrono3, chrono4;
+Chrono chronos[4];
 
-boolean passedFirstPost = false;
+bool passedFirstPost = false;
 
 String DadosInput = "";
-String TempValor, Md, MdAlr, Temp1, Temp2, Temp3, Temp4;
+String TempValor, Md, MdAlr;
 char CharInput[16], charMdAlr[4], charMd[2];
 int Modulo = 0;
 
-int Green1_opal, Green2_opal, Green3_opal, Green4_opal;
-int Grey1_opal, Grey2_opal, Grey3_opal, Grey4_opal;
-int Red1_opal, Red2_opal, Red3_opal, Red4_opal;
-int Yellou1_opal, Yellou2_opal, Yellou3_opal, Yellou4_opal;
-int Help1_opal, Help2_opal, Help3_opal, Help4_opal;
-int Temp1Back_opal, Temp2Back_opal, Temp3Back_opal, Temp4Back_opal;
-int Temp1_opal, Temp2_opal, Temp3_opal, Temp4_opal;
-int ImgTemp1_opal, ImgTemp2_opal, ImgTemp3_opal, ImgTemp4_opal;
+String Temps[4];
+int opacities[4][8];
 
-void setup() {  //------------------------------------------------------------------  SETUP -------------------------------------------------------
+void setup() {
   String title = "TouchFire_Control_Painel";
 
   Serial.begin(9600);
   Serial1.begin(9600);
 
-  chrono1.restart();
-  chrono2.restart();
-  chrono3.restart();
-  chrono4.restart();
+  for (auto &chrono : chronos) chrono.restart();
 
   Serial.println("Initializing board");
   Board *board = new Board();
@@ -47,7 +38,6 @@ void setup() {  //--------------------------------------------------------------
 
 #if LVGL_PORT_AVOID_TEARING_MODE
   auto lcd = board->getLCD();
-  // When avoid tearing function is enabled, the frame buffer number should be set in the board driver
   lcd->configFrameBufferNumber(LVGL_PORT_DISP_BUFFER_NUM);
 #if ESP_PANEL_DRIVERS_BUS_ENABLE_RGB && CONFIG_IDF_TARGET_ESP32S3
   auto lcd_bus = lcd->getBus();
@@ -59,121 +49,33 @@ void setup() {  //--------------------------------------------------------------
 #endif
   assert(board->begin());
 
-  //  Serial.println("Initializing LVGL");
   lvgl_port_init(board->getLCD(), board->getTouch());
 
   Serial.println("Creating UI");
-  /* Lock the mutex due to the LVGL APIs are not thread-safe */
   lvgl_port_lock(-1);
-
 
   ui_init();
   lvgl_port_unlock();
 }
 
-void loop() {  // ------------------------------------------------- LOOP ----------------- LOOP --------------------------------------------
-  /*  Serial.println("IDLE loop");
-    lv_label_set_text_fmt(ui_RXMess,"%s",DadosInput);
-    lv_label_set_text_fmt(ui_RXMess,"%s",DadosInput);
-    lv_label_set_text_fmt(ui_RXMess,"%s",DadosInput);
-    delay(1000);*/
-
-  // Read data from serial
- /* while (Serial1.available()) {
-    
-    DadosInput = Serial1.readString();
-    // char c = mySerial.read();
-    // DadosInput += c;
-    delay(10);
-  }
-*/
+void loop() {
   if (Serial1.available()) {
     DadosInput = Serial1.readString();
     delay(50);
   }
 
-  // Process received data if we have any
   if (DadosInput.length() > 0) {
-    //Serial.print("Received: ");
-   // Serial.println(DadosInput);
-
-    // Process data**********************************************************************************************   PARSER  ***************************
-    if (DadosInput.length() >= 10) {  // Check if we have enough data for parsing
-      NumMod();                       // Identify module number
-      Temp();                         // Extract temperature
-      ModAlrt();                      // Extract module alert
-
-      // Process based on module number
+    if (DadosInput.length() >= 10) {
+      NumMod();
+      Temp();
+      ModAlrt();
       processModuleData();
-    } else {
-     // Serial.println("Warning: Received data too short for processing");
     }
-    //*******************************************************---------------------------------******** DADOS PARA DISPLAY  ****************************************************
-
-    //**************************   MODULO 1  ***********************************
-    lv_label_set_text_fmt(ui_Temp1, "%s", Temp1);
-    lv_obj_set_style_opa(ui_Temp1, Temp1_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Gray1, Grey1_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Red1, Red1_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Yellou1, Yellou1_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Green1, Green1_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_HELP1, Help1_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_ImgTemp1, ImgTemp1_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Temp1Back, Temp1Back_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-
-
-
-    //**************************   MODULO 2  ***********************************
-    lv_label_set_text_fmt(ui_Temp2, "%s", Temp2);
-    lv_obj_set_style_opa(ui_Temp2, Temp2_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Gray2, Grey2_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Red2, Red2_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Yellou2, Yellou2_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Green2, Green2_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_HELP2, Help2_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_ImgTemp2, ImgTemp2_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Temp2Back, Temp2Back_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-
-
-
-    //**************************   MODULO 3  ***********************************
-    lv_label_set_text_fmt(ui_Temp3, "%s", Temp3);
-    lv_obj_set_style_opa(ui_Temp3, Temp3_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Gray3, Grey3_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Red3, Red3_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Yellou3, Yellou3_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Green3, Green3_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_HELP3, Help3_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_ImgTemp3, ImgTemp3_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Temp3Back, Temp3Back_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-
-
-    //**************************   MODULO 4  ***********************************
-    lv_label_set_text_fmt(ui_Temp4, "%s", Temp4);
-    lv_obj_set_style_opa(ui_Temp4, Temp4_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Gray4, Grey4_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Red4, Red4_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Yellou4, Yellou4_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Green4, Green4_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_HELP4, Help4_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_ImgTemp4, ImgTemp4_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_opa(ui_Temp4Back, Temp4Back_opal, LV_PART_MAIN | LV_STATE_DEFAULT);
-
-
-    //*************************************-------------------------------**********   DEBUG   ****************-------------------------------**************
-
-   //  lv_label_set_text_fmt(ui_Titulo_Touch_Fire, "%s", TempValor);
-
-
-    // Clear input data for next read
-    DadosInput = "  ";
+    DadosInput = "";
   }
-  // Check for module timeouts
   checkModuleTimeouts();
-
-  delay(50);  // Small delay to prevent CPU hogging
+  delay(50);
 }
-
 
 //***********************************************************  VOID   ***********************************************************************
 
@@ -222,271 +124,98 @@ void NumMod() {  // -----------------------------------------------------  Num M
   }
 }
 
-void processModuleData() {  //------------------------  VOID --Process Modulos Data -------------------------------------------------
-  switch (Modulo) {
-    case 1:  // Module 1
-      {
+void updateModuleStyles(int index, int opa[8]) {
+lv_obj_t *tempLabel = NULL;
+  lv_obj_t *grayImg = NULL;
+  lv_obj_t *redImg = NULL;
+  lv_obj_t *yellowImg = NULL;
+  lv_obj_t *greenImg = NULL;
+  lv_obj_t *helpLabel = NULL;
+  lv_obj_t *imgTemp = NULL;
+  lv_obj_t *tempBackLabel = NULL;
 
-        chrono1.restart();  // Reset timeout timer
+  switch(index) {
+  case 0:
+    tempLabel = ui_Temp1;
+    grayImg = ui_Gray1;
+    redImg = ui_Red1;
+    yellowImg = ui_Yellou1;
+    greenImg = ui_Green1;
+    helpLabel = ui_HELP1;
+    imgTemp = ui_ImgTemp1;
+    tempBackLabel = ui_Temp1Back;
+    break;
+  case 1:
+    tempLabel = ui_Temp2;
+    grayImg = ui_Gray2;
+    redImg = ui_Red2;
+    yellowImg = ui_Yellou2;
+    greenImg = ui_Green2;
+    helpLabel = ui_HELP2;
+    imgTemp = ui_ImgTemp2;
+    tempBackLabel = ui_Temp2Back;
+    break;
+  case 2:
+    tempLabel = ui_Temp3;
+    grayImg = ui_Gray3;
+    redImg = ui_Red3;
+    yellowImg = ui_Yellou3;
+    greenImg = ui_Green3;
+    helpLabel = ui_HELP3;
+    imgTemp = ui_ImgTemp3;
+    tempBackLabel = ui_Temp3Back;
+    break;
+  case 3:
+    tempLabel = ui_Temp4;
+    grayImg = ui_Gray4;
+    redImg = ui_Red4;
+    yellowImg = ui_Yellou4;
+    greenImg = ui_Green4;
+    helpLabel = ui_HELP4;
+    imgTemp = ui_ImgTemp4;
+    tempBackLabel = ui_Temp4Back;
+    break;
+  }
 
-        // Process alert level for Module 1
-        if (MdAlr == "M991A0") {  //--------------------------("Module 1 Alert 0");-------------------------------
-          Temp1 = TempValor;
-          Green1_opal = 255;  //VERDE
-          Grey1_opal = 0;
-          Red1_opal = 0;
-          Yellou1_opal = 0;
-          Temp1_opal = 255;
-          Temp1Back_opal = 255;
-          ImgTemp1_opal = 255;
-          Help1_opal = 0;
-
-
-        } else if (MdAlr == "M991A1") {  // -----------------------("Module 1 Alert 1");----------------------------------
-          Temp1 = TempValor;
-          Green1_opal = 0;
-          Grey1_opal = 0;
-          Red1_opal = 0;
-          Yellou1_opal = 255;  //  LARANGA
-          Temp1_opal = 255;
-          Temp1Back_opal = 255;
-          ImgTemp1_opal = 255;
-          Help1_opal = 0;
-
-
-        } else if (MdAlr == "M991A2") {  // -----------------------("Module 1 Alert 2");----------------------------------
-          Temp1 = TempValor;
-          Green1_opal = 0;
-          Grey1_opal = 0;
-          Red1_opal = 255;  // RED
-          Yellou1_opal = 0;
-          Temp1_opal = 255;
-          Temp1Back_opal = 255;
-          ImgTemp1_opal = 255;
-          Help1_opal = 0;
-
-        } else if (MdAlr == "M991A3") {  // -----------------------("Module 1 Alert MANUAL");----------------------------------
-          Green1_opal = 0;
-          Grey1_opal = 0;
-          Red1_opal = 255;  // RED
-          Yellou1_opal = 0;
-          Temp1_opal = 0;
-          Temp1Back_opal = 0;
-          ImgTemp1_opal = 0;
-          Help1_opal = 255;
-        }
-      }
-      break;
-
-    case 2:  // Module 2
-      {
-
-        chrono2.restart();  // Reset timeout timer
-
-        // Process alert level for Module 2
-        if (MdAlr == "M992A0") {  // -----------------------("Module 2 Alert 0");----------------------------------
-          Temp2 = TempValor;
-          Green2_opal = 250;  //VERDE
-          Grey2_opal = 1;
-          Red2_opal = 1;
-          Yellou2_opal = 1;
-          Temp2_opal = 250;
-          Temp2Back_opal = 250;
-          ImgTemp2_opal = 250;
-          Help2_opal = 1;
-
-
-        } else if (MdAlr == "M992A1") {  // -----------------------("Module 2 Alert 1");----------------------------------
-          Temp2 = TempValor;
-          Green2_opal = 0;
-          Grey2_opal = 0;
-          Red2_opal = 0;
-          Yellou2_opal = 255;  // LARANGA
-          Temp2_opal = 255;
-          Temp2Back_opal = 255;
-          ImgTemp2_opal = 255;
-          Help2_opal = 0;
-
-
-        } else if (MdAlr == "M992A2") {  // -----------------------("Module 2 Alert 2");----------------------------------
-          Temp2 = TempValor;
-          Green2_opal = 0;
-          Grey2_opal = 0;
-          Red2_opal = 255;  // RED
-          Yellou2_opal = 0;
-          Temp2_opal = 255;
-          Temp2Back_opal = 255;
-          ImgTemp2_opal = 255;
-          Help2_opal = 0;
-
-        } else if (MdAlr == "M992A3") {  // -----------------------("Module 2 Alert MANUAL ");----------------------------------
-          Green2_opal = 0;
-          Grey2_opal = 0;
-          Red2_opal = 255;  // RED
-          Yellou2_opal = 0;
-          Temp2_opal = 0;
-          Temp2Back_opal = 0;
-          ImgTemp2_opal = 0;
-          Help2_opal = 255;
-        }
-      }
-      break;
-
-    case 3:  // Module 1
-      {
-
-        chrono3.restart();  // Reset timeout timer
-
-        // Process alert level for Module 3
-        if (MdAlr == "M993A0") {  // -----------------------("Module 3 Alert 0");----------------------------------
-          Temp3 = TempValor;
-          Green3_opal = 255;  //VERDE
-          Grey3_opal = 0;
-          Red3_opal = 0;
-          Yellou3_opal = 0;
-          Temp3_opal = 255;
-          Temp3Back_opal = 255;
-          ImgTemp3_opal = 255;
-          Help3_opal = 0;
-
-        } else if (MdAlr == "M993A1") {  // -----------------------("Module 3 Alert 1");----------------------------------
-          Temp3 = TempValor;
-          Green3_opal = 0;
-          Grey3_opal = 0;
-          Red3_opal = 0;
-          Yellou3_opal = 255;  // LARANGA
-          Temp3_opal = 255;
-          Temp3Back_opal = 255;
-          ImgTemp3_opal = 255;
-          Help3_opal = 0;
-
-        } else if (MdAlr == "M993A2") {  // -----------------------("Module 3 Alert 2");----------------------------------
-          Temp3 = TempValor;
-          Green3_opal = 0;
-          Grey3_opal = 0;
-          Red3_opal = 255;  // RED
-          Yellou3_opal = 0;
-          Temp3_opal = 255;
-          Temp3Back_opal = 255;
-          ImgTemp3_opal = 255;
-          Help3_opal = 0;
-
-        } else if (MdAlr == "M993A3") {  // -----------------------("Module 3 Alert MANUAL  ");----------------------------------
-          Green3_opal = 0;
-          Grey3_opal = 0;
-          Red3_opal = 255;  // RED
-          Yellou3_opal = 0;
-          Temp3_opal = 0;
-          Temp3Back_opal = 0;
-          ImgTemp3_opal = 0;
-          Help3_opal = 255;
-        }
-      }
-      break;
-
-    case 4:  // Module 4
-      {
-
-        chrono4.restart();  // Reset timeout timer
-
-        // Process alert level for Module 4
-        if (MdAlr == "M994A0") {  // -----------------------("Module 4 Alert 0");----------------------------------
-          Temp4 = TempValor;
-          Green4_opal = 255;  //VERDE
-          Grey4_opal = 0;
-          Red4_opal = 0;
-          Yellou4_opal = 0;
-          Temp4_opal = 255;
-          Temp4Back_opal = 255;
-          ImgTemp4_opal = 255;
-          Help4_opal = 0;
-
-        } else if (MdAlr == "M994A1") {  // -----------------------("Module 4 Alert 1");----------------------------------
-          Temp4 = TempValor;
-          Green4_opal = 0;
-          Grey4_opal = 0;
-          Red4_opal = 0;
-          Yellou4_opal = 255;  // LARANGA
-          Temp4_opal = 255;
-          Temp4Back_opal = 255;
-          ImgTemp4_opal = 255;
-          Help4_opal = 0;
-
-
-        } else if (MdAlr == "M994A2") {  // -----------------------("Module 4 Alert 2");----------------------------------
-          Temp4 = TempValor;
-          Green4_opal = 0;
-          Grey4_opal = 0;
-          Red4_opal = 255;  // RED
-          Yellou4_opal = 0;
-          Temp4_opal = 255;
-          Temp4Back_opal = 255;
-          ImgTemp4_opal = 255;
-          Help4_opal = 0;
-
-        } else if (MdAlr == "M994A3") {  // -----------------------("Module 4 Alert MANUAL ");----------------------------------
-          Green4_opal = 0;
-          Grey4_opal = 0;
-          Red4_opal = 255;  // RED
-          Yellou4_opal = 0;
-          Temp4_opal = 0;
-          Temp4Back_opal = 0;
-          ImgTemp4_opal = 0;
-          Help4_opal = 255;
-        }
-      }
-      break;
-
-
-    default:
-     // Serial.println("Unknown module - no processing");
-      break;
+  lv_label_set_text_fmt(tempLabel, "%s", Temps[index]);
+  lv_obj_set_style_opa(tempLabel, opa[0], LV_PART_MAIN | LV_STATE_DEFAULT);
+  lv_obj_set_style_opa(grayImg, opa[1], LV_PART_MAIN | LV_STATE_DEFAULT);
+  lv_obj_set_style_opa(redImg, opa[2], LV_PART_MAIN | LV_STATE_DEFAULT);
+  lv_obj_set_style_opa(yellowImg, opa[3], LV_PART_MAIN | LV_STATE_DEFAULT);
+  lv_obj_set_style_opa(greenImg, opa[4], LV_PART_MAIN | LV_STATE_DEFAULT);
+  lv_obj_set_style_opa(helpLabel, opa[5], LV_PART_MAIN | LV_STATE_DEFAULT);
+  lv_obj_set_style_opa(imgTemp, opa[6], LV_PART_MAIN | LV_STATE_DEFAULT);
+  lv_obj_set_style_opa(tempBackLabel, opa[7], LV_PART_MAIN | LV_STATE_DEFAULT);
   }
 }
 
+void processModuleData() {
+  if (Modulo > 0 && Modulo <= 4) {
+    chronos[Modulo - 1].restart();
+    Temps[Modulo - 1] = TempValor;
+
+    if (MdAlr.endsWith("A0")) {
+      fillOpacity(Modulo - 1, {255, 0, 0, 0, 255, 255, 255, 0});  // VERDE
+    } else if (MdAlr.endsWith("A1")) {
+      fillOpacity(Modulo - 1, {0, 0, 0, 255, 255, 255, 255, 0});  // LARANJA
+    } else if (MdAlr.endsWith("A2")) {
+      fillOpacity(Modulo - 1, {0, 0, 255, 0, 255, 255, 255, 0});  // RED
+    } else if (MdAlr.endsWith("A3")) {
+      fillOpacity(Modulo - 1, {0, 0, 255, 0, 0, 0, 0, 255});  // MANUAL
+    }
+    updateModuleStyles(Modulo - 1, opacities[Modulo - 1]);
+  }
+}
+
+void fillOpacity(int index, std::initializer_list<int> opaList) {
+  std::copy(opaList.begin(), opaList.end(), opacities[index]);
+}
+
 void checkModuleTimeouts() {
-  // Check for module timeouts
-  if (chrono1.hasPassed(14000)) {  // ---------------------("Module 1 timeout");------------------------------------
-    Green1_opal = 0;
-    Grey1_opal = 255;  // GRAY
-    Red1_opal = 0;
-    Yellou1_opal = 0;
-    Temp1_opal = 0;
-    Temp1Back_opal = 255;
-    ImgTemp1_opal = 255;
-    Help1_opal = 0;
-  }
-
-  if (chrono2.hasPassed(14000)) {  // ---------------------("Module 2 timeout");------------------------------------
-    Grey2_opal = 250;              // GRAY
-    Red2_opal = 0;
-    Yellou2_opal = 0;
-    Green2_opal = 0;
-    Temp2_opal = 1;
-    Temp2Back_opal = 250;
-    ImgTemp2_opal = 250;
-    Help2_opal = 0;
-  }
-
-  if (chrono3.hasPassed(14000)) {  // --------------------------------("Module 3 timeout"); -------------------------------
-    Grey3_opal = 255;              // GRAY
-    Red3_opal = 0;
-    Yellou3_opal = 0;
-    Green3_opal = 0;
-    Temp3_opal = 0;
-    Temp3Back_opal = 255;
-    ImgTemp3_opal = 255;
-    Help3_opal = 0;
-  }
-
-  if (chrono4.hasPassed(14000)) {  // --------------------------------("Module 4 timeout"); -------------------------------
-    Grey4_opal = 255;              // GRAY
-    Red4_opal = 0;
-    Yellou4_opal = 0;
-    Green4_opal = 0;
-    Temp4_opal = 0;
-    Temp4Back_opal = 255;
-    ImgTemp4_opal = 255;
-    Help4_opal = 0;
+  for (int i = 0; i < 4; i++) {
+    if (chronos[i].hasPassed(14000)) {
+      fillOpacity(i, {0, 255, 0, 0, 0, 255, 255, 0});  // GRAY timeout
+      updateModuleStyles(i, opacities[i]);
+    }
   }
 }
